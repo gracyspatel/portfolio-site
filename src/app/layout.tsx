d532dfdcf4c8a35dcd41179ec7b9a4ef11be5159
Gracy Patel <gracyspatel@outlook.com>
2025-09-22 11:52:43 +0530

    imag tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
-import Footer from "@/components/footer";
+// import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Computer Science Engineer | ML Learner | Designer & Developer | Quietly Creative & Minimal</p>
             </div>
             <div className="flex gap-4 items-center flex-col sm:flex-row">
-              <a
+              <Link
                 className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
                 href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
                 target="_blank"
@@ -22,8 +23,8 @@ export default function Home() {
                   height={20}
                 />
                 View Projects
-              </a>
-              <a
+              </Link>
+              <Link
               //  dark:hover:bg-[#1a1a1a]
                 className="rounded-full border border-solid border-black/[.1]  transition-colors flex items-center justify-center gap-2 hover:bg-[#f2f2f2] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-auto"
                 href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -38,7 +39,7 @@ export default function Home() {
                   height={20}
                 />
                 Download Resume
-              </a>
+              </Link>
             </div>
           </div>
   );
